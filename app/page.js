0bbb0c420f2702d0490e7c1f427f952e5754d89f
Adam Jaggard <ajaggard@mux.com>
2023-12-11 15:54:00 +0000

    homepage link to playback page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <main>
-      ...
+      <h1>Mux Next.js backend example app</h1>
+      <p>At the moment, there's one example available: <a href="/signed-playback">Signed Playback</a></p>
     </main>
   )
 }

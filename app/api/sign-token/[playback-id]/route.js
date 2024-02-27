53ac77c1c83b58a11c682dade4f4db2bf3d45f1d
Adam Jaggard <ajaggard@mux.com>
2024-02-27 15:12:41 +0000

    upgrade to mux-node v8

--- app/api/sign-token/[playback-id]/route.js
+++ app/api/sign-token/[playback-id]/route.js
@@ -1,15 +1,17 @@
 import Mux from "@mux/mux-node";
 
 // This code assumes the following env vars are defined
+//   process.env.MUX_TOKEN_ID (signing key ID)
+//   process.env.MUX_TOKEN_SECRET (signing key ID)
 //   process.env.MUX_SIGNING_KEY (signing key ID)
 //   process.env.MUX_PRIVATE_KEY (signing key secret)
+const mux = new Mux();
 
 export async function GET(request, { params }) {
 	const playbackId = params['playback-id'];
-
-	const playbackToken = Mux.JWT.signPlaybackId(playbackId);
-	const thumbnailToken = Mux.JWT.signPlaybackId(playbackId, {type: 'thumbnail'});
-	const storyboardToken = Mux.JWT.signPlaybackId(playbackId, {type: 'storyboard'});
+	const playbackToken = await mux.jwt.signPlaybackId(playbackId);
+	const thumbnailToken = await mux.jwt.signPlaybackId(playbackId, {type: 'thumbnail'});
+	const storyboardToken = await mux.jwt.signPlaybackId(playbackId, {type: 'storyboard'});
     
 	return Response.json({
 		playbackToken,
